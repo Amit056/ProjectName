044f765aa39105c79c59487c76a7676fb825f175
akray <akray0413@gmail.com>
2023-01-07 20:27:33 +0530

    NEW COMMIT

---
+++ src/test/java/Example/Example2.java
@@ -0,0 +1,19 @@
+package Example;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Example2 {
+	public class Example1 {
+		@Test
+		public void example1()
+		{
+			int a=10;
+			int b=30;
+			System.out.println(a+b+"new project");
+		Reporter.log("pass", false);
+		Reporter.log("pass", a+b, false);
+		}
+	}
+
+}
