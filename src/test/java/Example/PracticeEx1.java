92799e1d5990b694cb70b85698c3231789e08383
akray <akray0413@gmail.com>
2023-01-07 20:04:43 +0530

    NEW YEAR 2023

---
+++ src/test/java/Example/PracticeEx1.java
@@ -0,0 +1,16 @@
+package Example;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeEx1 {
+	@Test
+	public void example1()
+	{
+		int a=10;
+		int b=30;
+		System.out.println(a+b+"new project");
+	Reporter.log("pass", false);
+	Reporter.log("pass", a+b, false);
+	}
+}
